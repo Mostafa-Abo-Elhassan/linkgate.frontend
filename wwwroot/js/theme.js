b0e590c1b48cac2d94893a0d5d2985f24ffd4c26
Mostafa-Abo-Elhassan <mostafamahmoudaboelhassn234@gmail.com>
2026-08-17 22:38:06 +0300

    Initial commit

---
+++ wwwroot/js/theme.js
@@ -0,0 +1,44 @@
+﻿// window.themeManager = {
+//     toggle: function () {
+//         const html = document.documentElement;
+
+//         const isDark = html.getAttribute("data-theme") === "dark";
+
+//         if (isDark) {
+//             html.removeAttribute("data-theme");
+//             return false;
+//         }
+
+//         html.setAttribute("data-theme", "dark");
+//         return true;
+//     }
+// };
+
+window.themeManager = {
+    toggle: function () {
+        const html = document.documentElement;
+
+        const isDark = html.getAttribute("data-theme") === "dark";
+
+        html.setAttribute(
+            "data-theme",
+            isDark ? "light" : "dark"
+        );
+
+        return !isDark;
+    }
+
+
+
+};
+
+
+window.languageManager = {
+    setLanguage: function (language) {
+        document.documentElement.lang = language;
+        document.documentElement.dir =
+            language === "ar" ? "rtl" : "ltr";
+
+        localStorage.setItem("linkgate-language", language);
+    }
+};
