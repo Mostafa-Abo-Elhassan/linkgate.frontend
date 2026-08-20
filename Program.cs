58ef425cf420dbc77e7d751d2f6b31bf451c7544
Mostafa-Abo-Elhassan <mostafamahmoudaboelhassn234@gmail.com>
2026-08-20 10:20:33 +0300

    done form application

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7185/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api-test-linkgate22.runasp.net/") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7185/") });
 builder.Services.AddScoped<ApplicationService>();
 
 await builder.Build().RunAsync();
