f3f05189ae1d5ee6a74f33ff9fb652c14b1ffef7
Mostafa-Abo-Elhassan <mostafamahmoudaboelhassn234@gmail.com>
2026-08-22 09:31:06 +0300

    done loader

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using LinkGate.frontend;
 using LinkGate.frontend.Services.Applications;
+using LinkGate.frontend.Services.Shared;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,6 +12,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api-test-linkgate22.runasp.net/") });
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7185/") });
-builder.Services.AddScoped<ApplicationService>();
+builder.Services.AddScoped<ApplicationService>(); 
+
+// أضف هذا السطر مع الـ Services الأخرى
+builder.Services.AddScoped<LoadingService>();
 
 await builder.Build().RunAsync();
---
+++ Services/Shared/LoadingService.cs
@@ -0,0 +1,18 @@
+﻿namespace LinkGate.frontend.Services.Shared
+{
+    public class LoadingService
+    {
+        // حدث يخبر المكونات عندما يتغير وضع التحميل
+        public event Action<bool, string>? OnLoadingStateChanged;
+
+        public void Show(string message = "جاري المعالجة...")
+        {
+            OnLoadingStateChanged?.Invoke(true, message);
+        }
+
+        public void Hide()
+        {
+            OnLoadingStateChanged?.Invoke(false, string.Empty);
+        }
+    }
+}
